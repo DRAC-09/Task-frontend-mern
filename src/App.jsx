b02f481360df9a8ef714c366a1a41d1e665af52c
AEstrada <allan.estrada9@gmail.com>
2023-11-01 17:42:54 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/tasks" element={<h1>Tasks Page</h1>} />
+          <Route path="/add-tasks" element={<h1>New Task</h1>} />
+          <Route path="/task/id" element={<h1>Update Task</h1>} />
+          <Route path="/profile" element={<h1>Profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
