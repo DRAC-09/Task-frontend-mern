f679df70aef809bcde31017494688d19f27c9a9f
drac-09 <allan.estrada9@gmail.com>
2024-01-28 22:36:37 -0600

    Mostrando token

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,15 +2,13 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const cookies = Cookies.get();
-const token = cookies.token;
+const token = "bearer" + cookies.token;
 
 const instance = axios.create({
   baseURL: `https://task-backend-mern-nu.vercel.app/api`,
   // baseURL: `http://localhost:${PORT}/api`,
-  headers: { token: token },
+  headers: { Authorization: `Bearer ${token}` },
   withCredentials: true,
 });
-
-console.log(token);
 
 export default instance;
