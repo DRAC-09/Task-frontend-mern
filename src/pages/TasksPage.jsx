154f8098ebec8668d099f9ba3638a90a161eb564
drac-09 <allan.estrada9@gmail.com>
2024-01-28 20:28:28 -0600

    Centrando texto xd

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -10,8 +10,12 @@ function TasksPage() {
     // console.log(tasks);
   }, []);
 
-  if (tasks.length === 0) return <h1>No tasks</h1>;
-
+  if (tasks.length === 0)
+    return (
+      <div className="flex h-[calc(100vh-100px)] w-screen items-center justify-center">
+        <h1>No tasks</h1>
+      </div>
+    );
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 justify-center items-start md:mx-10 py-5">
       {tasks.map((task) => (
