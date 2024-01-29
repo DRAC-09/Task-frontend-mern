dfcc6b0802ef6634c331290ffbff7d869881e6e0
drac-09 <allan.estrada9@gmail.com>
2024-01-28 20:36:11 -0600

    Añadiendo enlace backend vercel

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
-import { PORT } from "../config";
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `http://localhost:${PORT}/api`,
+  baseURL: `https://task-backend-mern-nu.vercel.app/api`,
+  // baseURL: `http://localhost:${PORT}/api`,
   withCredentials: true,
 });
 
